7d36b77009e65a11ae5f14dba2cd67d590c2b233
Your Name <you@example.com>
2018-11-19 16:04:46 +0100

    first commit

---
+++ src/timer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './timer.css';
+
+const Timer =() =>
+
+<div className='timer'>
+
+  <div className='timer-digits'>
+   00:30:00
+
+</div>
+
+<div className='timer-text'>
+
+  <h3 className='timer-hour'>Hour</h3>
+  <h3 className='timer-minute'>Minute</h3>
+  <h3 className='timer-second'>Second</h3>
+
+
+</div>
+
+
+
+
+
+
+
+
+</div>
+export default Timer
